31b9d9b2c77d92d824c617a3b571240cdcc83947
mati <mati@DESKTOP-9VEAPQP>
2021-05-20 12:20:31 +0200

    first Menu 0.1

---
+++ programaGastoEmpresa/src/programaGastoEmpresa/Menu.java
@@ -0,0 +1,28 @@
+package programaGastoEmpresa;
+
+public class Menu {
+	
+	public static void Menu(String []args) {
+		
+		System.out.println("1-Hi I'm your company manager to help you");
+		
+		System.out.println("_ _ _ _ _ _ _ \n");
+		
+		System.out.println("2-we can help you easily and safely. Create your account in two steps");
+		
+		System.out.println("3-Login to access your manager ");
+		
+		System.out.println("4-Quit if you've completed your appointments");
+		
+		System.out.println("Option:");
+	}
+	
+	public static void loginMenu() {
+	}
+	
+	public static void expensesMenu() {
+	}
+	
+	
+
+}
