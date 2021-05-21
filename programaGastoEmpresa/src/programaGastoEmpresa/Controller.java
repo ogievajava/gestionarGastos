b96acd89f37391f8be567258c636682cc7d0e70f
mati <mati@DESKTOP-5UOP2KP>
2021-05-21 13:46:03 +0200

    Version 0.1

--- programaGastoEmpresa/src/programaGastoEmpresa/Controller.java
+++ programaGastoEmpresa/src/programaGastoEmpresa/Controller.java
@@ -43,35 +43,6 @@ public class Controller {
 		
 		
 	}
-	/*
-	 * public static void AppStarting() {
-	 * 
-	 * // scope #2 Scanner reader = new Scanner(System.in);
-	 * 
-	 * String userToCreateAccount = reader.nextLine();
-	 * 
-	 * ArrayList<String> account = new ArrayList<String>();
-	 * 
-	 * account.add(userToCreateAccount);
-	 * 
-	 * 
-	 * boolean isValidated = Login.validateAccount(reader, account);
-	 * 
-	 * if (isValidated) {
-	 * 
-	 * Utils.printArray(account);
-	 * 
-	 * account = AccountUpdating.updateUser(reader, account); account =
-	 * AccountUpdating.updateEmail(reader, account); account =
-	 * AccountUpdating.updatePasswod(reader, account);
-	 * 
-	 * Utils.printArray(account);
-	 * 
-	 * // fake deleting ... account = AccountDeleting.deleteAccount(account);
-	 * 
-	 * Utils.printArray(account); isValidated = false; }
-	 * 
-	 * reader.close(); }
-	 */
+	
 
 }
