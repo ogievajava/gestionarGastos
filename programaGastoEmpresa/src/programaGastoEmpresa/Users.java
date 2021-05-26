3316128d1c9e94b20d2bbf5568698e681528cd9f
admin CIFO <admin CIFO@DESKTOP-340U509>
2021-05-26 12:38:17 +0200

    edit ArrayList Users 26/05/21

--- programaGastoEmpresa/src/programaGastoEmpresa/Users.java
+++ programaGastoEmpresa/src/programaGastoEmpresa/Users.java
@@ -1,7 +1,20 @@
 package programaGastoEmpresa;
 
-	public class Users {
-	
-	   
+import java.util.ArrayList;
+
+//Clase Users - Crearemos un Array de objetos User, Parquing de "User";
+
+public class Users {
+
+	public static void usersList(String name, String password, String email, String expense) {
+
+		ArrayList<String> user = new ArrayList<String>();
+
+		user.add(name);
+		user.add(password);
+		user.add(email);
+		user.add(expense);
+
 	}
 
+}
