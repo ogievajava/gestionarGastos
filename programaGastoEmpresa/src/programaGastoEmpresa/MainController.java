00ee6c56620c4d21b637d8f1b5c8a80baf8db28b
mati <mati@DESKTOP-9VEAPQP>
2021-05-28 09:58:58 +0200

    The MenuExpense is in process to create.. i try to do better

--- programaGastoEmpresa/src/programaGastoEmpresa/MainController.java
+++ programaGastoEmpresa/src/programaGastoEmpresa/MainController.java
@@ -1,5 +1,32 @@
 package programaGastoEmpresa;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+//import model.User;
+
 public class MainController {
+	
+	public static void AppStarting() {
+		
+		ArrayList<User>users = new ArrayList<User>();
+		Scanner reader = new Scanner(System.in);
+		
+		While (true){
+			
+			Menu.mainMenu();
+			String command = reader.nextLine();
+			if(command.equals("quit")) {
+				break;
+			}else if(command.equals("create")) {
+				UserCreation.createUser(reader,users);
+			}else {
+				System.out.println("Do you need to \"create\" an account or \"quit\");"
+			}
+			
+		}
+		
+		reader.close();
+	}
 
 }
