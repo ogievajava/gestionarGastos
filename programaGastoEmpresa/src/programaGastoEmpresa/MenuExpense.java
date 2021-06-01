4e2b284fe3832080c85216ff365c5d5ef9ea283f
mati <mati@DESKTOP-9VEAPQP>
2021-06-01 11:39:06 +0200

    MenuExpense v_ 0.2

---
+++ programaGastoEmpresa/src/programaGastoEmpresa/MenuExpense.java
@@ -0,0 +1,32 @@
+package programaGastoEmpresa;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+//import model.Expense;
+//import model.User;
+
+public class MenuExpense {
+
+	public static void createExpense(Scanner reader, User userValidated) {
+		 
+		System.out.println("1-Do you want to add something in Expense?");
+		
+		System.out.println("_ _ _ _ _ _ _ \n");
+		
+		System.out.println("2-You can modify something if you need");
+		
+		System.out.println("3-And delete if is necesary anytime ");
+		
+		//System.out.println("4-Quit if you've completed your appointments");
+		
+		System.out.println("Option:");
+	}
+	
+
+
+	
+}
+
